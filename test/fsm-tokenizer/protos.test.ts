293da45f09ea81b7643a6dff960a50044a273357
Tom Weatherhead <tomw@2hrd4u.org>
2022-01-07 10:45:45 -0700

    Dev

--- test/fsm-tokenizer/protos.test.ts
+++ test/fsm-tokenizer/protos.test.ts
@@ -2,9 +2,9 @@
 
 'use strict';
 
-import { LanguageSelector, LexicalAnalyzerSelector } from 'thaw-interpreter-types';
+import { LanguageSelector, LexicalAnalyzerSelector, LexicalState } from 'thaw-interpreter-types';
 
-import { createTokenizer } from '../..';
+import { createToken, createTokenizer } from '../..';
 
 test('Create Protos tokenizer', () => {
 	// Arrange
@@ -16,4 +16,22 @@ test('Create Protos tokenizer', () => {
 
 	// Assert
 	expect(actualValue).toBeTruthy();
+});
+
+test('Protos tokenize EOF test', () => {
+	// Arrange
+	const inputString = '';
+	const tokenizer = createTokenizer(
+		LexicalAnalyzerSelector.FiniteStateMachine,
+		LanguageSelector.Protos
+	);
+
+	// Act
+	const actualValue = tokenizer.tokenize(inputString);
+
+	// Assert
+	expect(actualValue.length).toBe(1);
+
+	// toBe() does a shallow comparison; toStrictEqual() does a deep comparison.
+	expect(actualValue).toStrictEqual([createToken(LexicalState.tokenEOF, 'EOF', 1, 1, false)]);
 });
